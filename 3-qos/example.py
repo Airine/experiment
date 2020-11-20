81f25f9f2696be9c032341957c2aedbac752fda6
aaron <airine0119@gmail.com>
2020-11-20 20:21:15 +0800

    dash log

--- 3-qos/example.py
+++ 3-qos/example.py
@@ -5,6 +5,7 @@ from urllib import request, parse
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 
 logging.getLogger().setLevel(logging.INFO)
@@ -27,14 +28,17 @@ def chrome_example():
     })
     logging.info('Prepared chrome options..')
 
+    dc = DesiredCapabilities.CHROME
+    dc['goog:loggingPrefs'] = { 'browser':'ALL' }
+    logging.info('Prepared desired capabilities..')
+
     #new
     chrome_driver = '/usr/bin/chromedriver'
     chrome_user_dir = '/usr/src/app/chrome_user_dir_real/'
     chrome_options.add_argument('--user-data-dir=' + chrome_user_dir)
     chrome_options.add_argument('--ignore-certificate-errors')
-    f= open("guru99.txt","w+")
 
-    browser = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
+    browser = webdriver.Chrome(chrome_driver, chrome_options=chrome_options, desired_capabilities=dc)
     logging.info('Initialized chrome browser..')
 
     browser.get(BASE_URL)
@@ -44,20 +48,68 @@ def chrome_example():
 
     logging.info('Page title: %s', browser.title)
 
+    logging.info('Processing log..')
+    init_delay = None
+    start = None
+    current_quality = 1
+    switches = 0
+    chunks = 0
+    total_quality = 0
+    stalls = 0
+
+    with open('log.txt','w+') as f:
+        for entry in browser.get_log('browser'):
+            msg = ' '.join(entry['message'].split(' ')[2:])
+
+            if not init_delay:
+                if '[PlaybackController]' in msg:
+                    if 'play ' in msg:
+                        start = int(msg.split(' ')[-2])
+                    elif 'playing' in msg:
+                        init_delay = (int(msg.split(' ')[-2]) - start)/1000
+            
+            if '[ScheduleController][video] OnFragmentLoadingCompleted' in msg:
+                chunks += 1
+                total_quality += current_quality
+                f.write('\n' + str(chunks) + ',' + msg[msg.find('Range:')+6:-2] + ',' + str(current_quality) + ',' + str(switches) + ',')
+
+            if '[PendingIndexMetrics]' in msg:
+                switches += 1
+                current_quality = int(msg.split(' ')[-1])
+            if '[IndexMetrics]' in msg:
+                current_quality = int(msg.split(' ')[-1])
+            
+            if '[Metrics]' in msg:
+                f.write(','.join(msg.split(' ')[-5:])+',')
+
+            if '[HttpMetrics]' in msg:
+                f.write(','.join([i.strip('"') for i in msg.split(' ')[-3:]])+',')
+            
+            if 'stalled' in msg:
+                stalls += 1
+        
+            # f.write(' '.join(msg.split(' ')[2:])+'\n')
+        avg_quality = total_quality/chunks
+
+        # average quality, number of switches, number of stalls, initial delay
+        f.write('\n'+str(avg_quality)+','+str(switches)+','+str(stalls)+','+str(init_delay)+'\n')
+        qoe = 0.25 * avg_quality - 0.25 * switches - 0.25 * stalls - 0.25 * init_delay
+        f.write('QoE:'+str(qoe))
+
     browser.quit()
     display.stop()
 
 
 def request_bandwidth():
-    with open("setting.json", "r") as file:
+    with open('setting.json', 'r') as file:
         data = json.load(file)
-        params = json.dumps(data).encode("utf8")
-        req = request.Request("http://192.168.1.241:5000/", data=params, headers={'content-type': 'application/json'})
+        params = json.dumps(data).encode('utf8')
+        req = request.Request('http://192.168.1.241:5000/', data=params, headers={'content-type': 'application/json'})
         res = request.urlopen(req)
         print(res.read().decode('utf8'))
 
 
 if __name__ == '__main__':
-    request_bandwidth()
-    sleep(5)
+    # request_bandwidth()
+    # sleep(5)
     chrome_example()
